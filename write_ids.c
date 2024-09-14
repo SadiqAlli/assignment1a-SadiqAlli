dfbb3937e4ba61113764a0cf7c81b88726108a8a
Sadiq Alli <allisadiq36@gmail.com>
2024-09-13 21:38:53 -0400

    assignment 1A

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "SadiqAlli";
+    char STUDENT_ID[] = "960574552";
 
     return 0 ;
 }
